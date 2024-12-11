f577203da474c4e0e9e962a36fea9adcdb6e6c17
Krati Agarwal <kratiag190@gmail.com>
2024-12-11 09:32:02 +0530

    corrected

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,33 @@
-import qBank from "./qBank"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-    <div id="Quiz">
-     <h1>my Quiz Application</h1>
-       <br/>
-       Question {qBank.id}.{qBank.question}<br/>
-       <input type="radio" name="opt"/>{qBank.option1}<br/> 
-       <input type="radio" name="opt"/>{qBank.option2}<br/> 
-       <input type="radio" name="opt"/>{qBank.option3}<br/> 
-       <input type="radio" name="opt"/>{qBank.option4}<br/>
-       <input type="button" value="Previous"/>
-       <input type="button" value="Next"/>
-       </div>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
--- react-counter-app/src/App.jsx
+++ react-counter-app/src/App.jsx
@@ -1,20 +1,35 @@
-import {Routes,Route} from 'react-router-dom'
-import Home from './Components/Home'
-import Counter from './Components/Counter'
-import Stopwatch from './Components/Stopwatch'
-import RefEx from './Components/RefEx'
-const App=()=>{
-  return(
-    <div>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/counter" element={<Counter/>}></Route>
-        <Route path="/stopwatch" element={<Stopwatch/>}></Route>
-        <Route path="/refex" element={<RefEx/>}></Route>
-        <Route path="*" element={<h1>No Page Available</h1>}></Route>
-      </Routes>
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-    </div>
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
+
 export default App
